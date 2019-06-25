776a3b1314742ea25db11dcaa1060636032cdccf
zhangsan <zhangsan@sina.com>
2019-06-25 16:56:26 +0800

    login reg

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -2,7 +2,7 @@ export default {
   debug: true,
   state: {
     is_new: false,
-    is_authenticated: window.localStorage.getItem('madblog-token') ? true : false
+    is_authenticated: window.localStorage.getItem('blog-token') ? true : false
   },
   setNewAction () {
     if (this.debug) { console.log('setNewAction triggered') }
@@ -18,7 +18,7 @@ export default {
   },
   logoutAction () {
     if (this.debug) console.log('logoutAction triggered')
-    window.localStorage.removeItem('madblog-token')
+    window.localStorage.removeItem('blog-token')
     this.state.is_authenticated = false
   }
 }
