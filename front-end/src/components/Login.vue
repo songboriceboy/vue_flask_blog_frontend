e7d2a3e56c7a509bc0cf12d8faeba9e9e4a6e614
zhangsan <zhangsan@sina.com>
2019-06-26 09:43:18 +0800

    user profile

--- front-end/src/components/Login.vue
+++ front-end/src/components/Login.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="container">
-    <alert 
-      v-if="sharedState.is_new"
-      v-bind:variant="alertVariant"
-      v-bind:message="alertMessage">
-    </alert>
     <h1>Sign In</h1>
     <div class="row">
       <div class="col-md-4">
@@ -26,73 +21,68 @@
     <br>
     <p>New User? <router-link to="/register">Click to Register!</router-link></p>
     <p>
-        Forgot Your Password?
-        <a href="#">Click to Reset It</a>
+      Forgot Your Password?
+      <a href="#">Click to Reset It</a>
     </p>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import Alert from './Alert'
-import store from '../store.js'
+  import store from '../store.js'
 
-export default {
-  name: 'Login',  //this is the name of the component
-  components: {
-    alert: Alert
-  },
-  data () {
-    return {
-      sharedState: store.state,
-      alertVariant: 'info',
-      alertMessage: 'Congratulations, you are now a registered user !',
-      loginForm: {
-        username: '',
-        password: '',
-        submitted: false,  // 是否点击了 submit 按钮
-        errors: 0,  // 表单是否在前端验证通过，0 表示没有错误，验证通过
-        usernameError: null,
-        passwordError: null
-      }
-    }
-  },
-  methods: {
-    onSubmit (e) {
-      this.loginForm.submitted = true  // 先更新状态
-      this.loginForm.errors = 0
-
-      if (!this.loginForm.username) {
-        this.loginForm.errors++
-        this.loginForm.usernameError = 'Username required.'
-      } else {
-        this.loginForm.usernameError = null
-      }
-
-      if (!this.loginForm.password) {
-        this.loginForm.errors++
-        this.loginForm.passwordError = 'Password required.'
-      } else {
-        this.loginForm.passwordError = null
-      }
-
-      if (this.loginForm.errors > 0) {
-        // 表单验证没通过时，不继续往下执行，即不会通过 axios 调用后端API
-        return false
-      }
-
-      const path = 'http://localhost:5000/api/tokens'
-      // axios 实现Basic Auth需要在config中设置 auth 这个属性即可
-      axios.post(path, {}, {
-        auth: {
-          'username': this.loginForm.username,
-          'password': this.loginForm.password
+  export default {
+    name: 'Login',  //this is the name of the component
+    data () {
+      return {
+        sharedState: store.state,
+        loginForm: {
+          username: '',
+          password: '',
+          submitted: false,  // 是否点击了 submit 按钮
+          errors: 0,  // 表单是否在前端验证通过，0 表示没有错误，验证通过
+          usernameError: null,
+          passwordError: null
         }
-      }).then((response) => {
+      }
+    },
+    methods: {
+      onSubmit (e) {
+        this.loginForm.submitted = true  // 先更新状态
+        this.loginForm.errors = 0
+
+        if (!this.loginForm.username) {
+          this.loginForm.errors++
+          this.loginForm.usernameError = 'Username required.'
+        } else {
+          this.loginForm.usernameError = null
+        }
+
+        if (!this.loginForm.password) {
+          this.loginForm.errors++
+          this.loginForm.passwordError = 'Password required.'
+        } else {
+          this.loginForm.passwordError = null
+        }
+
+        if (this.loginForm.errors > 0) {
+          // 表单验证没通过时，不继续往下执行，即不会通过 axios 调用后端API
+          return false
+        }
+
+        const path = '/tokens'
+        // axios 实现Basic Auth需要在config中设置 auth 这个属性即可
+        this.$axios.post(path, {}, {
+          auth: {
+            'username': this.loginForm.username,
+            'password': this.loginForm.password
+          }
+        }).then((response) => {
           // handle success
           window.localStorage.setItem('blog-token', response.data.token)
-          store.resetNotNewAction()
           store.loginAction()
+
+          const name = JSON.parse(atob(response.data.token.split('.')[1])).name
+          this.$toasted.success(`Welcome ${name}!`, { icon: 'fingerprint' })
 
           if (typeof this.$route.query.redirect == 'undefined') {
             this.$router.push('/')
@@ -100,16 +90,16 @@ export default {
             this.$router.push(this.$route.query.redirect)
           }
         })
-        .catch((error) => {
-          // handle error
-          if (error.response.status == 401) {
-            this.loginForm.usernameError = 'Invalid username or password.'
-            this.loginForm.passwordError = 'Invalid username or password.'
-          } else {
-            console.log(error.response)
-          }
-        })
+          .catch((error) => {
+            // handle error
+            if (error.response.status == 401) {
+              this.loginForm.usernameError = 'Invalid username or password.'
+              this.loginForm.passwordError = 'Invalid username or password.'
+            } else {
+              console.log(error.response)
+            }
+          })
+      }
     }
   }
-}
 </script>
